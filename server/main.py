98c89a5c5c3ad1bc038ff4c1024e460bfb7ec11a
Katy Zou <kzou@rocketmail.com>
2020-04-25 15:54:36 -0700

    mongo client python set up

--- server/main.py
+++ server/main.py
@@ -1,10 +1,16 @@
 from flask import Flask
 from flask_cors import CORS
+from pymongo import MongoClient
 
 app = Flask(__name__)
 CORS(app)
 
+#connect to mongo
+client = MongoClient("mongodb+srv://katyzou:abcde123@cluster0-mrgpe.gcp.mongodb.net/test?retryWrites=true&w=majority")
+
 
 @app.route('/')
 def hello_world():
-    return 'Hello, Mike!'
+    return 'Covid-19 Testing Scheduler'
+
+
